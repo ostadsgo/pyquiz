8e94917a4fc285067b43366bee82f9e2167e5c48
Saeed Gholami <saeed.ghollami@gmail.com>
2025-05-26 04:00:16 +0330

    Add basic scoring system.

--- app.js
+++ app.js
@@ -1,33 +1,103 @@
-let all_questions = [];
-var q_index = 0;
-for (const q of questions) {
-  let question = "<div class='question'>"
-  question += `<h3>${q.title}</h3>`
-  if (q.code) {
-    question += `<code><pre>${q.code}</pre></code>`
+/* TODO :: Disable next button if user not choiced any option.
+ * TODO :: Check user answer with correct answer
+ * TODO :: Create scoring system and page.
+ * TODO :: Add mechanizim to show if the selected value was correct or not.
+ */
+
+
+// Questin index to show in html page
+let questionIndex = 0;
+let score = 0;
+
+// Create 4 multi choice option out a question object
+function createOptions(options) {
+  let optionsHtml = "";
+  for (const option of options) {
+    optionsHtml += `
+    <label>
+       <input type="radio" class="option" name="option" value="${option}">${option}
+     </label><br>
+    `;
   }
-  let opts = "<div class='options'>";
-  for (const o of q.options) {
-    opts += `<label><input type="radio" name="q${q_index}" value="${o}">${o}</label><br>`
-  }
-  opts += "</div>";
-  question += opts;
-  q_index ++;
-  question += "</div>"
-  all_questions.push(question);
+  return optionsHtml;
 }
 
-let index = 0;
-qs = document.getElementById("questions");
-qs.innerHTML = all_questions[index++];
+// Create question out of a question object
+// A question consist of a title and 4 option to choose from
+function createQuestion(question) {
+  return `
+  <div class="question">
+    <h3 class="question-title">${question.title}</h3>
+    <pre class="question-code"><code>${question.code}</code></pre>
+    <div class="options">${createOptions(question.options)}</div>
+  </div>
+  `;
+}
 
-nextBtn = document.getElementById("next_btn");
-nextBtn.addEventListener("click", () => { 
-  qs = document.getElementById("questions");
-  qs.innerHTML = all_questions[index++];
-  if (index > all_questions.length) {
-    nextBtn.disabled = true;
+// Create html version of all questions.
+function createAllQuestions() {
+  // An array contain all question which are representable
+  // in html page.
+  let questions = [];
+  for (const questionObj of questionObjects) {
+    // get a question
+    let question = createQuestion(questionObj);
+    questions.push(question);
   }
-});
+  return questions;
+}
 
+// show a question on html page
+function showNextQuestion(questionsDiv, questions) {
+  questionsDiv.innerHTML = questions[questionIndex++];
+}
 
+// Get value of selected radio button.
+function getSelectedValue(options) {
+  for (const option of options) {
+    if (option.checked)
+      return option.value;
+  }
+}
+
+function getCorrectAnswer() {
+  // When script run it showes first question and questionIndex will
+  // inc by one so current question must decerase by one.
+  const currentQuestion = questionObjects[questionIndex - 1];
+  return currentQuestion.answer;
+}
+
+function calculateScore(selectedValue, correctAnswer) {
+  if (selectedValue == correctAnswer)
+    score += 1
+}
+
+// event handler when user clicked on nextButton in the html page
+function onNextButton(questionsDiv, questions) {
+  const options = questionsDiv.querySelectorAll(".option");
+  const selectedValue = getSelectedValue(options);
+  const correctAnswer = getCorrectAnswer();
+  calculateScore(selectedValue, correctAnswer);
+  // if there is question show next question
+  if (questionIndex < questionObjects.length){
+    showNextQuestion(questionsDiv, questions);
+    console.log(score);
+  }
+  // show test result
+  else {
+  }
+
+}
+
+// starting point of the script.
+function main() {
+  const questions = createAllQuestions();
+  const questionsDiv = document.getElementById("questions");
+  const nextButton = document.getElementById("next-button");
+  showNextQuestion(questionsDiv, questions);
+  nextButton.addEventListener("click", () =>
+    onNextButton(questionsDiv, questions),
+  );
+}
+
+main();
--- questions.js
+++ questions.js
@@ -1,9 +1,9 @@
-let questions = [
+let questionObjects = [
   {
     title: "کدام یک از گزینه های زیر نام مجاز برای متغیر می باشد؟",
     code: "",
     options: ["1_value", "data-row", "price_in_$_", "_output"],
-    answer: 3,
+    answer: "_output",
   },
   {
     title:
@@ -15,13 +15,13 @@ let questions = [
       "app_costs += 6.99",
       "هیچ کدام",
     ],
-    answer: 1,
+    answer: "appcosts + 6.99 = appcosts",
   },
   {
     title: "اگر تابعی دارای دستور return  نباشد چه مقداری برگشت میدهد؟",
     code: "",
     options: ["None", "0", "1", "False"],
-    answer: 0,
+    answer: "None",
   },
   {
     title: "خروجی تکه کد زیر چیست",
@@ -32,6 +32,6 @@ let questions = [
     print(result)
     `,
     options: ["0", "1", "2", "5"],
-    answer: 0,
+    answer: "0",
   },
 ];
